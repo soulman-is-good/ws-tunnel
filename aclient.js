aab4115aee6af3f0369186bd148f5e0b92f891f6
savinmax <savinmax@amazon.com>
2025-10-05 13:37:21 +0200

    Update code, add configurable config path

--- aclient.js
+++ aclient.js
@@ -1,92 +1,112 @@
-const WebSocket = require('ws');
-const net = require('net');
-const dgram = require('dgram');
-const url = require('url');
-const log4js = require('log4js');
-const { HttpProxyAgent } = require('http-proxy-agent');
-const { Code } = require('./src/consts/common');
-const { wrapBuffer, ctrlToString } = require('./src/utils/common');
-const config = require('./wclient_config.json');
+const WebSocket = require("ws");
+const net = require("net");
+const { readFileSync } = require("fs");
+const dgram = require("dgram");
+const url = require("url");
+const { parseArgs } = require("util");
+const log4js = require("log4js");
+const { HttpProxyAgent } = require("http-proxy-agent");
+const { Code } = require("./src/consts/common");
+const { wrapBuffer, ctrlToString } = require("./src/utils/common");
 
 // Allow wss connestion to self-signed certificate on server
 // WARNING: This make MitM attack possible. Dev only purpose
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
-log4js.configure(config.log.config);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const logger = log4js.getLogger("Client");
 const connections = [];
 const options = {};
 
-logger.level = config.log.level;
-
 if (process.env.HTTP_PROXY) {
   // TODO: replace deprecated parse
   options.agent = new HttpProxyAgent(url.parse(process.env.HTTP_PROXY));
 }
-
-config.paths.forEach(pathC => {
-  const { path, port, host, proto } = pathC;
-  const ws = new WebSocket(`wss://${config.host}:${config.port}${path}`, options);
-  let sock;
-
-  ws.on('open', function open() {
-    logger.info(`Connected to server on ${path}`);
+function main() {
+  const cmd = parseArgs({
+    strict: true,
+    allowPositionals: false,
+    args: process.argv.slice(2),
+    options: {
+      config: {
+        type: "string",
+        short: "c",
+        default: "./wclient_config.json",
+      },
+    },
   });
-  ws.on('message', data => {
-    const ctrl = data[0];
-    const sessionID = data.readUInt32BE(1);
-    const msg = data.subarray(5);
+  const config = JSON.parse(readFileSync(cmd.values.config, "utf8"));
+  log4js.configure(config.log.config);
+  logger.level = config.log.level;
+  config.paths.forEach((pathC) => {
+    const { path, port, host, proto } = pathC;
+    const ws = new WebSocket(
+      `wss://${config.host}:${config.port}${path}`,
+      options
+    );
     let sock;
 
-    logger.trace(sessionID, '=>', ctrlToString(ctrl));
-    if (ctrl === Code.CONNECT) {
-      sock = createClient(port, host, proto);
-      connections[sessionID] = sock;
+    ws.on("open", function open() {
+      logger.info(`Connected to server on ${path}`);
+    });
+    ws.on("message", (data) => {
+      const ctrl = data[0];
+      const sessionID = data.readUInt32BE(1);
+      const msg = data.subarray(5);
+      let sock;
 
-      if (proto === 'tcp') {
-        sock.on('data', data => {
-          ws.send(wrapBuffer(Code.MSG, sessionID, data), err => err && logger.error(err));
-        });
-        sock.on('close', () => {
-          ws.send(wrapBuffer(Code.CLOSE, sessionID));
-        });
-      } else {
-        logger.debug('INIT DGRAM');
-        sock.on('message', data => {
-          ws.send(wrapBuffer(Code.MSG, sessionID, data));
-        });
+      logger.trace(sessionID, "=>", ctrlToString(ctrl));
+      if (ctrl === Code.CONNECT) {
+        sock = createClient(port, host, proto);
+        connections[sessionID] = sock;
+
+        if (proto === "tcp") {
+          sock.on("data", (data) => {
+            ws.send(
+              wrapBuffer(Code.MSG, sessionID, data),
+              (err) => err && logger.error(err)
+            );
+          });
+          sock.on("close", () => {
+            ws.send(wrapBuffer(Code.CLOSE, sessionID));
+          });
+        } else {
+          logger.debug("INIT DGRAM");
+          sock.on("message", (data) => {
+            ws.send(wrapBuffer(Code.MSG, sessionID, data));
+          });
+        }
+        return;
       }
-      return;
-    }
-    sock = connections[sessionID];
+      sock = connections[sessionID];
 
-    if (!sock) {
-      logger.error('Socket is not opened', sessionID, path);
-      return;
-    }
-
-    if (proto === 'tcp') {
-      if (ctrl === Code.MSG) {
-        logger.debug('sending to tcp');
-        sock.write(msg);
-      } else {
-        logger.debug('closing tcp');
-        sock.end();
+      if (!sock) {
+        logger.error("Socket is not opened", sessionID, path);
+        return;
       }
-    } else {
-      sock.send(msg, port, host);
-    }
+
+      if (proto === "tcp") {
+        if (ctrl === Code.MSG) {
+          logger.debug("sending to tcp");
+          sock.write(msg);
+        } else {
+          logger.debug("closing tcp");
+          sock.end();
+        }
+      } else {
+        sock.send(msg, port, host);
+      }
+    });
+    ws.on("close", () => {
+      sock.end();
+    });
   });
-  ws.on('close', () => {
-    sock.end();
-  });
-});
+}
+main();
 
 function createClient(port, host, proto) {
-  if (proto === 'tcp') {
+  if (proto === "tcp") {
     return net.createConnection(port, host);
   }
 
-  return dgram.createSocket('udp4');
+  return dgram.createSocket("udp4");
 }
--- aserver.js
+++ aserver.js
@@ -1,102 +1,127 @@
-const https = require('https');
-const net = require('net');
-const dgram = require('dgram');
-const url = require('url');
-const fs = require('fs');
-const WebSocket = require('ws');
-const log4js = require('log4js');
-const { Code } = require('./src/consts/common');
-const { wrapBuffer, ctrlToString } = require('./src/utils/common');
-const config = require('./wserver_config.json');
-
-log4js.configure(config.log.config);
+const https = require("https");
+const net = require("net");
+const dgram = require("dgram");
+const url = require("url");
+const fs = require("fs");
+const {parseArgs} = require("util");
+const WebSocket = require("ws");
+const log4js = require("log4js");
+const { Code } = require("./src/consts/common");
+const { wrapBuffer, ctrlToString } = require("./src/utils/common");
 
 const logger = log4js.getLogger("Server");
 const connections = [];
 const data = {};
 const findFreeSessionID = () => {
-  const idx = connections.findIndex(conn => conn == null);
+  const idx = connections.findIndex((conn) => conn == null);
 
   return idx === -1 ? connections.length : idx;
 };
-const options = {
-  key: fs.readFileSync(config.key),
-  cert: fs.readFileSync(config.cert),
-};
-const server = https.createServer(options);
 
-logger.level = config.log.level;
-
-// init ports
-config.ports.forEach(portC => {
-  const path = `/${portC.port}${portC.proto[0]}`;
-  const port = portC.port;
-
-  data[path] = {
-    toWebSocket: buf => {
-      if (data[path].websocket) {
-        data[path].websocket.send(buf);
-      }
+function main() {
+  const cmd = parseArgs({
+    strict: true,
+    allowPositionals: false,
+    args: process.argv.slice(2),
+    options: {
+      config: {
+        type: "string",
+        short: "c",
+        default: "./wserver_config.json",
+      },
     },
-  };
-  data[path].path = path;
-  data[path].proto = portC.proto;
-  data[path].port = port;
-  data[path].wssrv = createWebSocketServer(data[path]);
-  data[path].server = createServer(portC.proto, port, data[path]);
-});
-
-server.on('connection', sock => log.debug('Connection from', sock.address()));
-
-// Setup main server
-server.on('upgrade', (request, socket, head) => {
-  const pathname = url.parse(request.url).pathname;
-  const conf = data[pathname];
-
-  if (!conf) {
-    return;
-  }
-  conf.wssrv.handleUpgrade(request, socket, head, function done(ws) {
-    conf.wssrv.emit('connection', ws, request);
   });
-});
+  const config = JSON.parse(fs.readFileSync(cmd.values.config, "utf8"));
+  log4js.configure(config.log.config);
 
-server.listen(config.port, config.host, () => {
-  logger.info('Websocket tunnel server is listening on port', server.address().port);
-  logger.info(' Following tunnels were created:');
-  Object.keys(data).forEach(path => {
-    const conf = data[path];
-    logger.info(`    => ${conf.proto} server on ${conf.port} port listeing web socket path ${server.address().address}:${server.address().port}${path}`);
+  const server = https.createServer({
+    key: fs.readFileSync(config.key),
+    cert: fs.readFileSync(config.cert),
   });
-});
 
+  logger.level = config.log.level;
+
+  // init ports
+  config.ports.forEach((portC) => {
+    const path = `/${portC.port}${portC.proto[0]}`;
+    const port = portC.port;
+
+    data[path] = {
+      toWebSocket: (buf) => {
+        if (data[path].websocket) {
+          data[path].websocket.send(buf);
+        }
+      },
+    };
+    data[path].path = path;
+    data[path].proto = portC.proto;
+    data[path].port = port;
+    data[path].wssrv = createWebSocketServer(data[path]);
+    data[path].server = createServer(portC.proto, port, data[path]);
+  });
+
+  server.on("connection", (sock) =>
+    logger.debug("Connection from", sock.address())
+  );
+
+  // Setup main server
+  server.on("upgrade", (request, socket, head) => {
+    const pathname = url.parse(request.url).pathname;
+    const conf = data[pathname];
+
+    if (!conf) {
+      return;
+    }
+    conf.wssrv.handleUpgrade(request, socket, head, function done(ws) {
+      conf.wssrv.emit("connection", ws, request);
+    });
+  });
+
+  server.listen(config.port, config.host, () => {
+    logger.info(
+      "Websocket tunnel server is listening on port",
+      server.address().port
+    );
+    logger.info(" Following tunnels were created:");
+    Object.keys(data).forEach((path) => {
+      const conf = data[path];
+      logger.info(
+        `    => ${conf.proto} server on ${
+          conf.port
+        } port listeing web socket path ${server.address().address}:${
+          server.address().port
+        }${path}`
+      );
+    });
+  });
+}
 // Helper functions
 function createServer(proto, port, conf) {
   let server;
 
-  if (proto === 'tcp') {
+  if (proto === "tcp") {
     server = net.createServer();
     // TODO: Make listening host configurable1
-    server.listen(port, '0.0.0.0');
-    server.on('error', logger.error);
-    server.on('connection', sock => {
+    server.listen(port, "0.0.0.0");
+    server.on("error", logger.error);
+    server.on("connection", (sock) => {
       const sessionID = findFreeSessionID();
       connections[sessionID] = sock;
       conf.toWebSocket(wrapBuffer(Code.CONNECT, sessionID));
-      sock.on('data', msg => {
+      sock.on("data", (msg) => {
         conf.toWebSocket(wrapBuffer(Code.MSG, sessionID, msg));
       });
-      sock.on('close', () => {
+      sock.on("close", () => {
         conf.toWebSocket(wrapBuffer(Code.CLOSE, sessionID));
       });
     });
   } else {
-    server = dgram.createSocket('udp4');
-    server.bind(port, '0.0.0.0');
-    server.on('error', logger.error);
+    server = dgram.createSocket("udp4");
+    server.bind(port, "0.0.0.0");
+    server.on("error", logger.error);
     const sessionID = findFreeSessionID();
     connections[sessionID] = server;
-    server.on('message', (buf, rinfo) => {
+    server.on("message", (buf, rinfo) => {
       connections[sessionID].host = rinfo.address;
       connections[sessionID].port = rinfo.port;
       conf.toWebSocket(wrapBuffer(Code.MSG, sessionID, buf));
@@ -104,43 +129,47 @@ function createServer(proto, port, conf) {
   }
 
   return server;
-
 }
 
 function createWebSocketServer(conf) {
   const srv = new WebSocket.Server({ noServer: true });
 
-  srv.on('connection', (sock, req) => {
+  srv.on("connection", (sock, req) => {
     conf.websocket = sock;
     conf.request = req;
 
-    if (req.url.endsWith('u')) {
+    if (req.url.endsWith("u")) {
       initDatagramm(sock);
     }
-    sock.on('message', msg => {
+    sock.on("message", (msg) => {
       const ctrl = msg[0];
       const sessionID = msg.readUInt32BE(1);
       const cli = connections[sessionID];
 
       if (!cli) {
-        logger.error('Wrong session ID, no connection there', sessionID, 'from', sock.url);
+        logger.error(
+          "Wrong session ID, no connection there",
+          sessionID,
+          "from",
+          sock.url
+        );
         return;
       }
       const data = msg.subarray(5);
-      logger.trace(sessionID, '=>', ctrlToString(ctrl));
+      logger.trace(sessionID, "=>", ctrlToString(ctrl));
 
       if (ctrl === Code.MSG) {
-        if (conf.proto === 'tcp') {
+        if (conf.proto === "tcp") {
           cli.write(data);
         } else {
           cli.send(data, cli.port, cli.host);
         }
       } else if (ctrl === Code.CLOSE) {
-        logger.debug('Closing connection')
+        logger.debug("Closing connection");
         cli.end();
         connections[sessionID] = null;
       } else {
-        logger.error('Unknown control byte', ctrl, 'from', sock.url);
+        logger.error("Unknown control byte", ctrl, "from", sock.url);
       }
     });
   });
@@ -153,5 +182,7 @@ function initDatagramm(sock) {
     if (conn instanceof dgram.Socket) {
       sock.send(wrapBuffer(Code.CONNECT, sessionID));
     }
-  })
+  });
 }
+
+main();
